5805e01c5f88c538f9395583e0f9964d7dcd1493
Beqael <beqaelashvili3@gmail.com>
2025-02-08 16:26:45 +0400

    add_expenses_page_and_retrieving_sullary

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,7 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/expense", expenseRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
